4526fd2a51c8f83e929ce07865d19151b7e679b1
Jonathan Ballands <jballands@gmail.com>
2013-10-30 11:59:32 -0400

    Completed programming the API caller class!

---
+++ ClarityForAndroid/src/com/clarityforandroid/controllers/ModifyPatientActivity.java
@@ -0,0 +1,21 @@
+package com.clarityforandroid.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The controller that controls the modification of a patient.
+ * 
+ * @author Jonathan Ballands
+ * @version 1.0
+ */
+public class ModifyPatientActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	
+		// TODO: Do something...
+	}
+	
+}
